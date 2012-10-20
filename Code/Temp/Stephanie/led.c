5f72d109361c33f3b8a6030c80fb796d64b734a9
skrapil <skrapil@6a2b6605-3038-4f4b-ab73-e3b1860e81fc>
2012-10-20 04:18:32 +0000

    Added red/green led code

---
+++ Code/Temp/Stephanie/led.c
@@ -0,0 +1,22 @@
+#include "led.h"
+
+void ledInit() {
+   sbi(DDRB, RED_LED);
+   sbi(DDRB, GREEN_LED);
+}
+
+void greenLedOn() {
+   sbi(PORTB, GREEN_LED);
+}
+
+void greenLedOff() {
+   cbi(PORTB, GREEN_LED);
+}
+
+void redLedOn() {
+   sbi(PORTB, RED_LED);
+}
+
+void redLedOff() {
+   cbi(PORTB, RED_LED);
+}
---
+++ Code/Temp/Stephanie/led.h
@@ -0,0 +1,14 @@
+#include "globals.h"
+
+#define RED_LED 2
+#define GREEN_LED 3
+
+void ledInit();
+
+void greenLedOn();
+
+void greenLedOff();
+
+void redLedOn();
+
+void redLedOff();
