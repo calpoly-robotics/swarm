53cb1bee9aeaa050cf335059903d0b1656090cff
Scott Techau <scottbot95@gmail.com>
2013-10-31 19:25:41 -0700

    Added base files

---
+++ Robot/Drivers/drivers.h
@@ -0,0 +1,10 @@
+#ifndef DRIVERS_H_
+
+/**
+ * Wrapper for all the init functions for the various drivers
+ */
+inline void initDrivers() {
+	// TODO there's nothing here yet..
+}
+
+#endif /* DRIVERS_H_*/
