338c84fe1fbf4dc33ca12a6a9a1b7d0b7963cc42
Scott Techau <scottbot95@gmail.com>
2013-11-01 20:44:44 -0700

    Added basic code for the motors. Need to figure out what the pins mean...

---
+++ Robot/Drivers/motor.c
@@ -0,0 +1,38 @@
+#include "motor.h"
+
+void initMotor() 
+{
+
+}
+
+void leftMotor(s16 speed)
+{
+	if (speed > 0 ) {
+		cbi(PORTD, 3); // AIN1
+		sbi(PORTC, 2); // AIN2
+		OCR2B = speed;
+	} else if (spped < 0) {
+		sbi(PORTD, 3);
+		cbi(PORTC, 2);
+		OCR2B = (u08) (speed*-1);
+	} else {
+		cbi(PORTD, 3);
+		cbi(PORTC, 2);
+	}
+}
+
+void rightMotor(s16 speed)
+{
+	if (speed > 0 ) {
+		cbi(PORTD, 5); // AIN1
+		sbi(PORTC, 0); // AIN2
+		OCR2A = speed;
+	} else if (spped < 0) {
+		sbi(PORTD, 5);
+		cbi(PORTC, 0);
+		OCR2A = (u08) (speed*-1);
+	} else {
+		cbi(PORTD, 5);
+		cbi(PORTC, 0);
+	}
+}
---
+++ Robot/Drivers/motor.h
@@ -0,0 +1,11 @@
+#ifndef MOTOR_H_
+#define MOTOR_H_
+
+#include "../globals.h"
+
+void initMotor();
+
+void leftMotor(s16 speed);
+void rigthMotor(s16 speed);
+
+#endif /* MOTOR_H_ */
