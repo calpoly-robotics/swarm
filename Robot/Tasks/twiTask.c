68246d4a743af536aa276efed235206db2a8bbf6
Scott Techau <scottbot95@gmail.com>
2013-11-17 20:05:32 -0800

    i think transmittion is done

--- Robot/Tasks/twiTask.c
+++ Robot/Tasks/twiTask.c
@@ -1,40 +1,62 @@
 #include "twiTask.h"
+#include "../Drivers/twi.h"
+#include "../Drivers/eeprom.h"
 #include <util/twi.h>
+
+#define TWACK() (TWCR=(1<<TWINT)|(1<<TWEN)|(1<<TWEA))
+#define TWNACK() (TWCR=(1<<TWINT)|(1<<TWEN))
+#define TWRESET() (TWCR=(1<<TWINT)|(1<<TWEN)|(1<<TWSTO)|(1<<TWEA))
 
 #define TWI_RECV_BUFLEN 10
 
 static volatile Task* myTask;
 
 // 0=read 1=write
-volatile u08 twiMode = 0;
+u08 twiMode = 0;
 
 volatile u08 recvIndex = 0;
 volatile u08 twiData[TWI_RECV_BUFLEN];
 
 // 0 indicates that we haven't sent anything
-volatile u08 txIndex = 0;
+u08 txIndex = 0;
+size_t txLen = 0;
+u08* txData;
 
 void enableTwiTask() {
 	myTask = addTask(runTWI);
 	(*myTask).interval = 100;
+	enableTwi();
 }
 
 void disableTwiTask() {
 	removeTask((*myTask).index);
+	disableTwi();
 }
 /**
- * preps system to send data, if we have any
+ * loads any data we might have into 
  */
 void handleReadRequest() {
 	if (txIndex==0) {
-		// TODO: 
+		txData = eepromReadLog(&txLen);
 	}
+
+	TWDR = txData[txIndex++];
+	if (txIndex == txLen)
+		TWNACK();
+	else
+		TWACK();
+		
 }
 
 void handleWriteComplete() {
 	if (recvIndex > 0) { // this should always be the case
 		switch (twiData[0]) {
+		case WHOS_THERE:	// we don't need to reply to this ourselves
+			TWACK();		// the microcontroller does it for us
+			break;
 
+		default:
+			TWACK();	
 		}
 	}
 }
@@ -46,19 +68,17 @@ void runTWI() {
 		case TW_SR_SLA_ACK: 		// SLA+W received, ACK returned
 			recvIndex = 0;		// reset the buffer index
 			twiMode = 1;		// save that we are being written to
-			sbi(TWCR, TWEA);
+			TWACK();
 			break;
 
 		case TW_SR_DATA_ACK:		// data received, ACK returned
 			twiData[recvIndex] = TWDR;
 			if (++recvIndex == TWI_RECV_BUFLEN)
-				cbi(TWCR, TWEA);
+				TWNACK();
 
 		case TW_SR_STOP:			// stop or repeated start condition received
 			if (twiMode)			// twiMode=1 when writting
 				handleWriteComplete();
-
-			sbi(TWCR, TWEA);
 			break;
 
 		case TW_ST_SLA_ACK:			// SLA+R received, ACK returned
@@ -77,7 +97,5 @@ void runTWI() {
 			break;
 		}
 
-		// we have to do this manually... :(
-		sbi(TWCR, TWINT); 		// clear interrupt flag
 	}
 }
